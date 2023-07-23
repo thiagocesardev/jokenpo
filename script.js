c07c439bd11849d63a9d9ba7709029fc1a55c9df
devthiagocesar <devthiagocesar@gmail.com>
2023-07-23 10:07:14 -0300

    230723.

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
-const playhuman = (humanChoice) =>{
-    console.log (humanChoice)
-    console.log ('retorno' + playMachine())
+const result = document.querySelector ('.result')
+const humanScore = document.querySelector ('#human-score')
+const machineScore = document.querySelector ('#machine-score')
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+const playHuman = (humanChoice) =>{
+ 
+    playTheGame (humanChoice, playMachine())
+
 }
 
-const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
-    const randomNumber = Math.floor(Math.random()*3)
+
+const playMachine = () =>{
+    const choices = [`rock`, `paper`, `scissors`]
+    const randomNumber = Math.floor (Math.random()*3)
     return choices [randomNumber]
 }
 
-
-const playTheGame = (human, machine)=>{
-
+const playTheGame = (human, machine) =>{
+console.log ("Humano:" + human +  " Maquina:" + machine)
+if (human === machine){
+    result.innerHTML = "Deu empate!"
+} else  if ((human === 'paper' && machine === 'rock') ||
+            (human === 'rock' && machine === 'scissors') ||        
+            (human === 'scissors' && machine === 'paper')
+){
+    humanScoreNumber++
+    humanScore.innerHTML = humanScoreNumber
+    result.innerHTML = "Você ganhou!"
+} else{
+    machineScoreNumber++
+    machineScore.innerHTML = machineScoreNumber
+    result.innerHTML = "Você perdeu para a Alexa"
 }
+}
+
+
+
 
 
 /*
 papel -> pedra -> tesoura
-*/
-/*
-
-
-const playhuman = (humanChoice) => {
-    console.log(humanChoice)
-
-
-    console.log('retorno' + playMachine())
-}
-
-const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
-    const randomNumber = Math.floor(Math.random() * 3)
-  
-    return choices[randomNumber]
-}
-
-
-const playTheGame = (human, machine) =>{
-    console.log ('Humano: ' + human + 'Maquina: ' + machine)
-}
-
 */
