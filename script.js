b0ea67499894a635f2ad28163c2f21973201e242
devthiagocesar <devthiagocesar@gmail.com>
2023-07-23 18:37:05 -0300

    230723...

--- script.js
+++ script.js
@@ -1,45 +1,34 @@
-const result = document.querySelector ('.result')
-const humanScore = document.querySelector ('#human-score')
-const machineScore = document.querySelector ('#machine-score')
+const result = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('##machine-score')
 
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
-const playHuman = (humanChoice) =>{
- 
-    playTheGame (humanChoice, playMachine())
+const playHuman = (humanChoice) => {
+    playTheGame(humanChoice, playMachine())
+}
+const playMachine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * 3)
 
+    return choices[randomNumber]
 }
 
+const playTheGame = (human, machine) => {
+    if (human === machine) {
+        result.innerHTML = "Deu empate"
 
-const playMachine = () =>{
-    const choices = [`rock`, `paper`, `scissors`]
-    const randomNumber = Math.floor (Math.random()*3)
-    return choices [randomNumber]
+    } else if ((human === 'paper' && machine === 'rock') ||
+        (human === 'rock' && machine === 'scissors') ||
+        (human === 'scissors' && machine === 'paper')) {
+        result.innerHTML = "Você ganhou!"
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+
+    } else {
+        result.innerHTML = "Máquina ganhou!"
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+    }
 }
-
-const playTheGame = (human, machine) =>{
-console.log ("Humano:" + human +  " Maquina:" + machine)
-if (human === machine){
-    result.innerHTML = "Deu empate!"
-} else  if ((human === 'paper' && machine === 'rock') ||
-            (human === 'rock' && machine === 'scissors') ||        
-            (human === 'scissors' && machine === 'paper')
-){
-    humanScoreNumber++
-    humanScore.innerHTML = humanScoreNumber
-    result.innerHTML = "Você ganhou!"
-} else{
-    machineScoreNumber++
-    machineScore.innerHTML = machineScoreNumber
-    result.innerHTML = "Você perdeu para a Alexa"
-}
-}
-
-
-
-
-
-/*
-papel -> pedra -> tesoura
-*/
