17fc4ce2e6e12ed290a691e31734ba6983ba88da
devthiagocesar <devthiagocesar@gmail.com>
2023-07-23 09:10:38 -0300

    230723

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const playhuman = (humanChoice) =>{
+    console.log (humanChoice)
+    console.log ('retorno' + playMachine())
+}
+
+const playMachine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random()*3)
+    return choices [randomNumber]
+}
+
+
+const playTheGame = (human, machine)=>{
+
+}
+
+
+/*
+papel -> pedra -> tesoura
+*/
+/*
+
+
 const playhuman = (humanChoice) => {
     console.log(humanChoice)
 
@@ -16,3 +39,5 @@ const playMachine = () => {
 const playTheGame = (human, machine) =>{
     console.log ('Humano: ' + human + 'Maquina: ' + machine)
 }
+
+*/
